a707a21196af9b5bd2759b1cd643d8e0a7979408
fabriziojuarez <fabriziojuarez79@gmail.com>
2025-06-05 21:11:05 -0500

    correccion de valor_mayor_posible: comprueba la cantidad del billete

--- main.php
+++ main.php
@@ -1,60 +1,49 @@
 <?php
 
-
 $billetes = [
     ['valor' => 10, 'cantidad' =>100],
-    ['valor' => 20, 'cantidad' =>100],
+    ['valor' => 20, 'cantidad' =>0],
     ['valor' => 50, 'cantidad' =>100],
     ['valor' => 100, 'cantidad' => 100],
 ];
 
+function mayor_valor_posible(int $monto){
+    $mayor_valor_posible = 0;
+    foreach($GLOBALS['billetes'] as $billete){
+        if($monto > $billete['valor']){
+            if($billete['valor'] > $mayor_valor_posible){
+                if($billete['cantidad'] !== 0){
+                    $mayor_valor_posible = $billete;
+                }
+            }
+        }
+    }
+    return $mayor_valor_posible;
+}
+
+function cantidad_posible(int $monto, int $valor_billete){
+    $billete_mayor = mayor_valor_posible($monto);
+    $cantidad_billete = $billete_mayor['cantidad'];
+    if($cantidad_billete == 0){
+        $GLOBALS['billetes'] = array_diff($GLOBALS['billetes'], $billete_mayor);
+    }
+    $cantidad_usada = 0;
+    while($monto > $billete_mayor['valor']){
+        $monto = $monto - $billete_mayor['valor'];
+        $cantidad_usada = $cantidad_usada + 1;
+    }
+
+}
+
+$monto = 80;
+
 $salida = [];
 
-function billete_mayor(int $monto){
-    foreach($GLOBALS['billetes'] as $billete){
-        if($monto >= $billete['valor']){
-            $billete_mayor = $billete;
-        }
-    }
-    return $billete_mayor;
-}
+echo $GLOBALS['monto'];
 
-function cantidad_billete(int $monto, int $valor_billete){
-    $cantidad = 0;
-    $conteo = true;
-    while($conteo){
-        if($monto < $valor_billete){
-            $conteo = false;
-        }
-        $monto = $monto-$valor_billete;
-        $cantidad = $cantidad + 1;
-    }
-    $cantidad = $cantidad - 1;
-    return $cantidad;
-}
+$result = mayor_valor_posible(40);
 
-function menor_cantidad_posible(int $monto){
-    $billete_mayor = billete_mayor($monto);
-    $cantidad = cantidad_billete($monto, $billete_mayor['valor']);
-    ajustar_cantidad($billete_mayor,$cantidad);
-    $billete = [
-        'valor' => $billete_mayor['valor'],
-        'cantidad' => $cantidad,
-    ];
-    $nuevo_monto=$monto-$billete_mayor['valor']*$cantidad;
-    
-    array_push($GLOBALS['salida'], $billete, menor_cantidad_posible($nuevo_monto));
-}
+echo var_dump($result);
 
-function ajustar_cantidad(array $billete_seleccionado, int $cantidad_usada){
-    for($i=0; $i<count($GLOBALS['billetes']); $i++){
-        if($GLOBALS['billetes'][$i] == $billete_seleccionado){
-            $GLOBALS['billetes'][$i]['cantidad'] = $GLOBALS['billetes'][$i]['cantidad']-$cantidad_usada;
-        }
-    }
-}
-
-menor_cantidad_posible(710);
-//$prueba = mayor_valor($billetes);
-
-echo var_dump($GLOBALS['billetes']);
+echo var_dump(mayor_valor_posible($GLOBALS['monto']));
+//echo var_dump($GLOBALS['billetes']);
